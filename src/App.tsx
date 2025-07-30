4c496c3f816c0daf2d29eb569b3ca79a1e03cb0a
GasterBlaster951 <wdgaster951@gmail.com>
2025-07-30 10:19:26 -0300

    classe-ususario-joao-40-anos

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Usuario} from './models/Usuario.ts';
 function App() {
 
   const u1 = new Usuario('João Da Silva', 37);
-  
+  u1.idade = 40;
   return (
     <div>
     <p>{u1.apresentar()}</p>
